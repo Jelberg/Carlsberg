44e33d95e907e2553039b564fdc7511ee8ad666a
Jessica Elberg <elbergjessica@gmail.com>
2017-01-04 20:51:39 -0400

    Drops montados

---
+++ SCRIPTS/DROP.sql
@@ -0,0 +1,69 @@
+/*==============================================================*/
+/* DBMS name:      ORACLE Version 11g                           */
+/* Created on:     4/1/2017 8:40:21 p.m.                        */
+/*==============================================================*/
+
+
+drop table CATALOGO_PROVEEDOR_EQ cascade constraints;
+
+drop table CATALOGO_PROVEEDOR_MP cascade constraints;
+
+drop table CERVEZA cascade constraints;
+
+drop table CIUDAD cascade constraints;
+
+drop table COMPOSICION cascade constraints;
+
+drop table CONTRATO cascade constraints;
+
+drop table DESCUENTOPEDIDIOPARAPRODUCCION cascade constraints;
+
+drop table DETALLE_PEDIDO cascade constraints;
+
+drop table DISTRIBUCION_CERVEZA cascade constraints;
+
+drop table EMPRESA cascade constraints;
+
+drop table ENVIOS cascade constraints;
+
+drop table ESTILO cascade constraints;
+
+drop table FABRICA cascade constraints;
+
+
+
+drop table MAQUINARIA cascade constraints;
+
+drop table MATERIA_PRIMA cascade constraints;
+
+drop table PAGO cascade constraints;
+
+drop table PAIS cascade constraints;
+
+drop table PEDIDO cascade constraints;
+
+drop table PRESENTACION cascade constraints;
+
+drop table PRESENTACION_CERVEZA cascade constraints;
+
+drop table PRODUCCIONMENSUAL cascade constraints;
+
+drop table PROVEEDORES cascade constraints;
+
+drop table VARIEDAD cascade constraints;
+
+drop table V_C cascade constraints;
+
+--TDA
+/*
+drop table COMIDA cascade constraints;
+drop table CONTACTO cascade constraints;
+drop table DATOSCUENTA cascade constraints;
+drop table DISTRIBUCION cascade constraints;
+drop table FOMA_PAGO cascade constraints;
+drop table MEDIDA cascade constraints;
+drop table PATROCINIO cascade constraints;
+drop table RECETAPROCESOBASICO cascade constraints;
+drop table RESULTADOEVALUACION cascade constraints;
+drop table RESUMENHISTORICO cascade constraints;
+*/
